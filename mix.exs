4dd043846fd24bf5421de4685334511da0af57f4
Jesper Dalberg <jda@fullrate.dk>
2016-08-08 01:13:01 +0200

    version bumped

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ACS.Mixfile do
 
   def project do
     [app: :acs_ex,
-     version: "0.0.2",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
