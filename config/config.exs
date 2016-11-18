65a936c21a52af1d964ea8a573234a0d80663e51
Jesper Dalberg <jda@fullrate.dk>
2016-11-18 09:59:20 +0100

    New option: acs_ip6

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,7 @@ use Mix.Config
 
 config :acs_ex,
   acs_port: 7548,
-  acs_ip: {0, 0, 0, 0, 0, 0, 0, 0}
+  acs_ip: {0, 0, 0, 0},
+  acs_ip6: {0, 0, 0, 0, 0, 0, 0, 0}
 
 import_config "#{Mix.env}.exs"
